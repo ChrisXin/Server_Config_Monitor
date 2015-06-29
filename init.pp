5224619734d8296c06309dbfba093e6d0228bdeb
Xin (Chris) Zou <x9zou@uwaterloo.ca>
2015-06-29 00:28:40 -0400

    Create init.pp

---
+++ init.pp
@@ -0,0 +1,11 @@
+class wm_install::block21_monitor_server_config
+{
+  #Execute a script to send an email of the diff for the server.sh file
+  exec { 'Notify Admin server config changes':
+    command =>'/home/webmethods/dist/email_server_config_changes.sh',
+    cwd     => '/home/webmethods/dist',
+    group   => webmethods,
+    user    => webmethods,
+    timeout => 15000,
+    before => file['/home/ocanada.sh']
+  }
